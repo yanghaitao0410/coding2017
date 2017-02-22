075f4e2d54626216960c92d925480fe34c25cdd5
yanght1454385822@qq.com <yanght1454385822@qq.com>
2017-02-22 15:08:34 +0800

    ArrayList can't test

---
+++ group06/1454385822/src/com/coding/basic/ArrayList.java
@@ -0,0 +1,94 @@
+package com.coding.basic;
+
+import java.util.Arrays;
+
+public class ArrayList implements List {
+	
+	private int size = 0;
+	
+	private int pos = 0; // 当前数组的末尾元素的下一位置
+	
+	private Object[] elementData = new Object[100];
+	
+	public void add(Object o){
+	
+		if(pos >= elementData.length - 1){
+			//数组扩容1/3
+			Arrays.copyOf(elementData, elementData.length + elementData.length/3);
+		}
+		elementData[pos++] = o;
+		
+	}
+	public void add(int index, Object o){
+
+		if(pos >= elementData.length - 1){
+			//数组扩容1/3
+			Arrays.copyOf(elementData, elementData.length + elementData.length/3);
+		}
+		/*
+		 * 情况1.index < pos && pos < elementData.length - 1 
+		 * index 只能在pos前面
+		 */
+		 if(index < pos && pos < elementData.length - 1){
+			 Object[] tem = new Object[pos - index];
+			 System.arraycopy(elementData, index, tem, 0, tem.length);
+			 elementData[index] = o;
+			 System.arraycopy(tem, 0, elementData, index + 1, tem.length);
+			 pos++;
+		 }else{
+			 throw new IndexOutOfBoundsException();
+		 }
+		
+		
+
+	}
+	
+	public Object get(int index){
+		if(index < pos){
+			return elementData[index];
+		}
+		throw new IndexOutOfBoundsException();
+	}
+	
+	public Object remove(int index){
+		Object result;
+		//将数字删除并将该数字后面的元素向前移动一位
+		if(index < pos ){ //只有index在pos之前才进行删除操作
+			result = elementData[index];
+			if(pos - index > 1){ //删除的不是最后一个元素
+				Object[] tem = new Object[pos - index - 1];
+				System.arraycopy(elementData, index + 1, tem, 0, tem.length);
+				for(int i=0; i<tem.length; i++){
+					elementData[index + i] = tem[i];
+				}
+			}
+			elementData[--pos] = null;
+			return result;
+		}
+		throw new IndexOutOfBoundsException();
+	}
+	
+	public int size(){
+		return pos - 1;
+	}
+	
+	public Iterator iterator(){
+		return new Iterator(){
+			private int cur = 0;
+			@Override
+			public boolean hasNext() {
+				if(cur < pos){
+					return true;
+				}
+				return false;
+			}
+
+			@Override
+			public Object next() {
+				return elementData[cur++];
+			}
+			
+		};
+	}
+	
+}
